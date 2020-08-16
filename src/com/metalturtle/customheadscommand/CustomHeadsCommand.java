fa7121575aa8a8399f54dde760662d69420767e8
dorian <dekolis1218@gmail.com>
2020-08-16 12:08:22 -0700

    Work on command (more finished, still not)

--- src/com/metalturtle/customheadscommand/CustomHeadsCommand.java
+++ src/com/metalturtle/customheadscommand/CustomHeadsCommand.java
@@ -1,27 +1,35 @@
 package com.metalturtle.customheadscommand;
 
+import com.metalturtle.customheadscommand.commands.Commands;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Server;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CustomHeadsCommand extends JavaPlugin {
 
     FileConfiguration config = this.getConfig();
-
-    public FileConfiguration getConfigurationFile() {
-        return this.getConfig();
-    }
+    public static Material paymentItem;
+    public static int paymentAmount;
+    public Server server = getServer();
 
     @Override
     public void onEnable() {
         config.addDefault("payment", "diamond");
-        config.addDefault("amount", "2");
+        config.addDefault("amount", 2);
         config.options().copyDefaults(true);
-        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Custom Heads Command Plugin Loaded!");
+        saveConfig();
+        paymentItem = Material.valueOf(config.getString("payment").toUpperCase());
+        paymentAmount = config.getInt("amount");
+        getCommand("customhead").setExecutor(new Commands());
+        server.getConsoleSender().sendMessage(ChatColor.GREEN + "Custom Heads Command Plugin Loaded!");
+        server.getConsoleSender().sendMessage(ChatColor.BLUE + "The set payment amount is " + paymentAmount);
     }
 
     @Override
     public void onDisable() {
-        getServer().getConsoleSender().sendMessage(ChatColor.RED + "Custom Heads Command Plugin Unloaded!");
+        server.getConsoleSender().sendMessage(ChatColor.RED + "Custom Heads Command Plugin Unloaded!");
     }
 }
--- src/com/metalturtle/customheadscommand/commands/Commands.java
+++ src/com/metalturtle/customheadscommand/commands/Commands.java
@@ -1,11 +1,9 @@
 package com.metalturtle.customheadscommand.commands;
 
 import com.metalturtle.customheadscommand.CustomHeadsCommand;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -19,16 +17,13 @@ public class Commands implements CommandExecutor {
         Player player = (Player) commandSender;
 
         if (command.getName().equalsIgnoreCase("customhead")) {
-            CustomHeadsCommand plugin = new CustomHeadsCommand();
-            FileConfiguration config = plugin.getConfigurationFile();
-            String paymentItem = config.getString("payment");
-            int paymentAmount = config.getInt("amount");
-            ItemStack payment = new ItemStack(Material.valueOf(paymentItem), paymentAmount);
-            if (player.getInventory().contains(payment)) {
-
+            ItemStack payment = new ItemStack(CustomHeadsCommand.paymentItem);
+            if (player.getInventory().containsAtLeast(payment, CustomHeadsCommand.paymentAmount)) {
+                player.sendMessage("You have the payment");
             }
+            return true;
         }
-
+        player.sendMessage("You do not have the payment");
         return true;
     }
 }
