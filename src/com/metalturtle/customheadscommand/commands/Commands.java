602b626a51cfac1f99c3d1505103b02cd553d8b0
dorian <dekolis1218@gmail.com>
2020-08-17 12:01:34 -0700

    Finish command

--- src/com/metalturtle/customheadscommand/commands/Commands.java
+++ src/com/metalturtle/customheadscommand/commands/Commands.java
@@ -1,13 +1,13 @@
 package com.metalturtle.customheadscommand.commands;
 
 import com.metalturtle.customheadscommand.CustomHeadsCommand;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
 public class Commands implements CommandExecutor {
 
@@ -26,13 +26,22 @@ public class Commands implements CommandExecutor {
             }
             // Get the item set in the config file
             ItemStack payment = new ItemStack(CustomHeadsCommand.paymentItem);
-            // Check player's inventory for the payment
-            if (player.getInventory().containsAtLeast(payment, CustomHeadsCommand.paymentAmount)) {
-                player.sendMessage(ChatColor.GREEN + "You paid " + CustomHeadsCommand.paymentAmount + " x " + CustomHeadsCommand.paymentItemString + " for the head named " + args[0]);
+            int paymentAmount = CustomHeadsCommand.paymentAmount;
+            ItemStack fullPayment = new ItemStack(CustomHeadsCommand.paymentItem, paymentAmount);
+            // If the player is in creative mode they do not need to pay
+            if (player.getGameMode() == GameMode.CREATIVE) {
                 giveHead(player, args[0]);
+                player.sendMessage(ChatColor.GREEN + "You got the head named " + args[0]);
                 return true;
             }
-            player.sendMessage(ChatColor.RED + "You do not have sufficient payment. The set payment for this command is " + CustomHeadsCommand.paymentAmount + " x " + CustomHeadsCommand.paymentItemString + ".");
+            // Check player's inventory for the payment
+            if (player.getInventory().containsAtLeast(payment, paymentAmount)) {
+                player.sendMessage(ChatColor.GREEN + "You paid " + paymentAmount + " x " + CustomHeadsCommand.paymentItemString + " for the head named " + args[0]);
+                giveHead(player, args[0]);
+                player.getInventory().removeItem(fullPayment);
+                return true;
+            }
+            player.sendMessage(ChatColor.RED + "Insufficient funds. The set payment for this command is " + CustomHeadsCommand.paymentAmount + " x " + CustomHeadsCommand.paymentItemString + ".");
             return true;
         }
         return true;
@@ -40,7 +49,10 @@ public class Commands implements CommandExecutor {
     // Method to give the player head to the player
     private void giveHead(Player player, String headName) {
         ItemStack head = new ItemStack(Material.PLAYER_HEAD);
-        //head.setData();
+        SkullMeta meta = (SkullMeta) Bukkit.getItemFactory().getItemMeta(Material.PLAYER_HEAD);
+        OfflinePlayer playerName = player.getServer().getOfflinePlayer(headName);
+        meta.setOwningPlayer(playerName);
+        head.setItemMeta(meta);
         player.getInventory().addItem(head);
 
     }
